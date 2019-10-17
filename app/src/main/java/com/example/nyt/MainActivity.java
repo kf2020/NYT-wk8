8a4a361ba6bd3c33a687fccba018675a334db345
amisrs <gvn.cim@gmail.com>
2019-10-17 23:54:46 +1100

    Update for Week 5
    Put things into fragments and add BottomNavigationView

--- app/src/main/java/com/example/nyt/MainActivity.java
+++ app/src/main/java/com/example/nyt/MainActivity.java
@@ -1,151 +1,91 @@
 package com.example.nyt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    // Not needed here anymore
-//    private ImageView bookmarkButton1;
-//    private ImageView bookmarkButton2;
-//    private ImageView shareButton1;
-//    private ImageView shareButton2;
-//    private TextView headline1;
-//    private TextView headline2;
-//    private TextView details1;
-//    private TextView details2;
-//    boolean button1State = false;
-//    boolean button2State = false;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
+// Notice the Activity implements the interface OnFragmentInteractionListener, meaning this Activity
+// MUST have the method defined by the interface (see bottom), and if it does then this Activity
+// can be considered an OnFragmentInteractionListener; it listens for Fragment interaction.
+// This is only relevant to ProfileFragment in this app (because ArticleRecyclerFragment has nothing
+// to listen to).
+public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.rv_main);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        // I want there to be a Fragment in the slot from the start
+        Fragment fragment = new ArticleRecyclerFragment();
+        swapFragment(fragment);
 
-        ArticleAdapter articleAdapter = new ArticleAdapter();
+        bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // menuItem = the item on the bottom nav view that was selected
+                // The id's here belong to the items in the menu of the BottomnNavigationView
+                // The menu is chunked out as bottom_nav_menu.xml in the res > menu folder
+                if (menuItem.getItemId() == R.id.nav_articles) {
+                    Fragment fragment = new ArticleRecyclerFragment();
+                    swapFragment(fragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.nav_books) {
+                    // TODO: Implement a books fragment
+                    return true;
+                } else if (menuItem.getItemId() == R.id.nav_profile) {
+                    Fragment fragment = new ProfileFragment();
 
-        // We wrote our setData method to be like a setter, so we give our ArrayList from
-        // FakeDatabase to the Adapter.
-        articleAdapter.setData(FakeDatabase.getAllArticles());
-        recyclerView.setAdapter(articleAdapter);
+                    // Here's just an example of passing information to the Fragment via Bundle
+                    Bundle bundle = new Bundle();
+                    bundle.putString("param1", "First Argument");
+                    bundle.putString("param2", "Second Argument");
+                    fragment.setArguments(bundle);
+                    // End bundle part
 
-        // We don't need any of this stuff below here anymore
-        // We've automated it by writing our custom Adapter
-        // Look at the code in ArticleAdapter onBindViewHolder, and compare it what we used to have
-        // here.
+                    swapFragment(fragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 
-//        ConstraintLayout article1 = findViewById(R.id.article1);
-//        ConstraintLayout article2 = findViewById(R.id.article2);
-//
-//        headline1 = article1.findViewById(R.id.newsHeadline);
-//        headline2 = article2.findViewById(R.id.newsHeadline);
-//        details1 = article1.findViewById(R.id.newsDetails);
-//        details2 = article2.findViewById(R.id.newsDetails);
-//
-//        bookmarkButton1 = article1.findViewById(R.id.newsSaveButton);
-//        bookmarkButton2 = article2.findViewById(R.id.newsSaveButton);
-//
-//        shareButton1 = article1.findViewById(R.id.newsShareButton);
-//        shareButton2 = article2.findViewById(R.id.newsShareButton);
-//
-//
-//        // Getting objects from the FakeDatabase and filling in the TextViews with data
-//        final Article articleObject1 = FakeDatabase.getArticleById(4);
-//        final Article articleObject2 = FakeDatabase.getArticleById(3);
-//        articleObject1.setImageDrawableId(R.drawable.news_headline_photo); // don't worry about images for now
-//        articleObject2.setImageDrawableId(R.drawable.news_headline_photo);
-//
-//        headline1.setText(articleObject1.getHeadline());
-//        headline2.setText(articleObject2.getHeadline());
-//
-//        details1.setText(articleObject1.getSummary());
-//        details2.setText(articleObject2.getSummary());
-//
-//        // Clicking on the article to go to detail activity
-//        article1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), ArticleDetailActivity.class);
-//                intent.putExtra("ArticleID", articleObject1.getArticleID());
-//                startActivity(intent);
-//            }
-//        });
-//
-//        article2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), ArticleDetailActivity.class);
-//                intent.putExtra("ArticleID", articleObject2.getArticleID());
-//                startActivity(intent);
-//            }
-//        });
-//
-//        // Bookmark buttons
-//        bookmarkButton1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getApplicationContext(), "Click1 Registered", Toast.LENGTH_SHORT).show();
-//                if(!button1State) {
-//                    bookmarkButton1.setImageResource(R.drawable.ic_bookmark_black_24dp);
-//                } else {
-//                    bookmarkButton1.setImageResource(R.drawable.ic_bookmark_border_black_24dp);
-//                }
-//
-//                button1State = !button1State;
-//            }
-//        });
-//
-//        bookmarkButton2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getApplicationContext(), "Click2 Registered", Toast.LENGTH_SHORT).show();
-//                if(!button2State) {
-//                    bookmarkButton2.setImageResource(R.drawable.ic_bookmark_black_24dp);
-//                } else {
-//                    bookmarkButton2.setImageResource(R.drawable.ic_bookmark_border_black_24dp);
-//                }
-//
-//                button2State = !button2State;
-//            }
-//        });
-//
-//        // Share button implicit intents
-//        shareButton1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(Intent.ACTION_SEND);
-//
-//                intent.putExtra(Intent.EXTRA_TEXT, articleObject1.getHeadline());
-//                intent.setType("text/plain");
-//                startActivity(intent);
-//            }
-//        });
-//
-//        shareButton2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(Intent.ACTION_SEND);
-//
-//                intent.putExtra(Intent.EXTRA_TEXT, articleObject2.getHeadline());
-//                intent.setType("text/plain");
-//                startActivity(intent);
-//            }
-//        });
+    /**
+     * Helper method to change the fragment displayed in the activity. We put this here so we don't
+     * have to repeat the code every time we want to saw
+     * @param fragment: instance of the fragment to go into the slot
+     */
+    private void swapFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_slot, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * If the Fragment has recognised this Activity as a listener, the Fragment can cause events
+     * that this Activity can listen to (and thus call this method when it hears the event).
+     * @param string
+     */
+    @Override
+    public void onFragmentInteraction(String string) {
+        Toast.makeText(this, "Hello! I have come from the fragment. Message: " + string, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showCoolMessage(String string) {
+        Toast.makeText(this, "wow you are so " + string, Toast.LENGTH_SHORT).show();
 
     }
 }
